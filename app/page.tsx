4ec15494bce03df1a5fc5728b2ba299e64901395
codehass <hassan.elouardy06@gmail.com>
2025-06-03 23:04:27 +0100

    add page and sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import Navbar from "./ui/Navbar";
+import Features from "./ui/Features";
 
 export default function Home() {
   return (
-    <div >
+    <>
       <Navbar />
-      This is finwase-clone page
-    </div>
+      <main className="flex flex-col items-center justify-center">
+      <Features />
+      
+      <section id="pricing" className="flex flex-col items-center justify-center h-screen bg-yellow-300">
+        Pricing section 
+      </section>
+      <section id="testimonials" className="flex flex-col items-center justify-center h-screen bg-green-300">
+      Testimonials section 
+      </section>
+    </main>
+    </>
   );
 }
