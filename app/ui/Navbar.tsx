d30154c89f6fe8a886db5f2169f0f2a3e32d12b6
codehass <hassan.elouardy06@gmail.com>
2025-06-03 23:04:55 +0100

    edit navbar

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between py-8 px-4">
-      <div className="flex items-center justify-center gap-2">
-        <p className="w-7 h-7 bg-black rounded-full">.</p>
-        <p className="text-2xl font-bold">Finwise</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <Link href="/" className="text-lg ">Features</Link>
-        <Link href="/" className="text-lg">Pricing</Link>
-        <Link href="/" className="text-lg">Testimonials</Link>
-        <button className="bg-yellow-300 px-8 py-3 rounded-3xl">
-          Download
-        </button>
+    <nav className="flex justify-around w-full">
+      <div className="flex items-center justify-between py-8 px-4 mx-6">
+        <div className="flex items-center justify-center gap-2">
+          <p className="w-7 h-7 bg-black rounded-full">.</p>
+          <p className="text-2xl font-bold">Finwise</p>
+        </div>
+        <div className="md:flex items-center gap-4 hidden ">
+          <Link href="#features" className="text-lg ">Features</Link>
+          <Link href="#pricing" className="text-lg">Pricing</Link>
+          <Link href="#testimonials" className="text-lg">Testimonials</Link>
+          <button className="bg-yellow-300 px-8 py-3 rounded-3xl">
+            Download
+          </button>
+          </div>
         </div>
     </nav>
   )
