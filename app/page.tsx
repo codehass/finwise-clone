56b67d04e758dc0036aa5570e4dac874982127c7
codehass <hassan.elouardy06@gmail.com>
2025-06-15 11:51:47 +0100

    formate code using prettier

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import Features from "./ui/Features";
 import Pricing from "./ui/Pricing";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main className="flex flex-col items-center justify-center">
-      <Features />
-      <Pricing />
-      <section id="testimonials" className="flex flex-col items-center justify-center h-screen bg-green-300">
-      Testimonials section 
-      </section>
-    </main>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<main className="flex flex-col items-center justify-center">
+				<Features />
+				<Pricing />
+				<section
+					id="testimonials"
+					className="flex flex-col items-center justify-center h-screen bg-green-300"
+				>
+					Testimonials section
+				</section>
+			</main>
+		</>
+	);
 }
--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link";
 
-
 export default function Navbar() {
-  return (
-      <nav className="flex w-full justify-between py-8 px-8">
-        <div className="flex items-center justify-center gap-2">
-          <p className="w-7 h-7 bg-black rounded-full">.</p>
-          <p className="text-2xl font-bold">Finwise</p>
-        </div>
-        <div className="md:flex items-center gap-4 hidden ">
-          <Link href="#features" className="text-lg hover:text-gray-500">Features</Link>
-          <Link href="#pricing" className="text-lg hover:text-gray-500">Pricing</Link>
-          <Link href="#testimonials" className="text-lg hover:text-gray-500">Testimonials</Link>
-          <button className="cursor-pointer bg-yellow-300 hover:bg-yellow-400 px-8 py-3 rounded-3xl">
-            Download
-          </button>
-        </div>
-      </nav>
-    
-  )
+	return (
+		<nav className="flex w-full justify-between py-8 px-8">
+			<div className="flex items-center justify-center gap-2">
+				<p className="w-7 h-7 bg-black rounded-full">.</p>
+				<p className="text-2xl font-bold">Finwise</p>
+			</div>
+			<div className="md:flex items-center gap-4 hidden ">
+				<Link href="#features" className="text-lg hover:text-gray-500">
+					Features
+				</Link>
+				<Link href="#pricing" className="text-lg hover:text-gray-500">
+					Pricing
+				</Link>
+				<Link href="#testimonials" className="text-lg hover:text-gray-500">
+					Testimonials
+				</Link>
+				<button className="cursor-pointer bg-yellow-300 hover:bg-yellow-400 px-8 py-3 rounded-3xl">
+					Download
+				</button>
+			</div>
+		</nav>
+	);
 }
