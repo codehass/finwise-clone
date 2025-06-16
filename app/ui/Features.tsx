6eeee84abc8c0564b734386d96801cdbd4dd96f1
codehass <hassan.elouardy06@gmail.com>
2025-06-16 21:46:10 +0100

    update features section

--- app/ui/Features.tsx
+++ app/ui/Features.tsx
@@ -1,60 +1,54 @@
+import { ChartNoAxesColumn, Target, TrendingUp } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Features() {
 	return (
 		<section
 			id="features"
-			className="flex flex-col items-center justify-start gap-4 h-screen w-full md:max-w-2xl bg-blue-300 pt-20"
+			className="flex flex-col items-center justify-start gap-4 w-full pt-20"
 		>
-			<h1 className="font-bold text-4xl md:text-6xl lg:text-6xl text-center">
-				Smart, Secure, Simple Financial Management
-			</h1>
-			<p className="text-center my-6 mx-9 text-xl md:text-2xl">
-				From effortless budgeting to real-time investment insights, Finwise puts
-				you in control of your money like never before
-			</p>
-			<div className="flex gap-4 my-6">
-				<button className="flex items-center gap-3 bg-black text-white px-6 rounded-3xl h-14">
-					<Image
-						src="/appel.svg"
-						alt="Download from App Store"
-						width={30}
-						height={30}
-						className="text-white"
-					/>
+			<div className="flex flex-col-reverse md:flex-row">
+				<Image src="/mockup-1.webp" alt="features" width={500} height={300} />
+				<div className="flex flex-col items-center justify-center gap-8">
+					<h3 className="text-3xl font-bold text-center">Smart Budgeting</h3>
+					<p className="text-center text-gray-700">
+						Take the guesswork out of managing your money. Our AI-powered
+						budgeting tool adapts to your lifestyle and helps you stay on track.
+					</p>
 
-					<div>
-						<p className="text-xs">Download on the</p>
-						<Link href="/" className="text-xl font-bold">
-							App Store
-						</Link>
+					<div className="flex flex-col justify-center items-center text-center gap-3">
+						<ChartNoAxesColumn />
+						<div>
+							<h2 className="text-2xl font-semibold">
+								Intelligent Categorization
+							</h2>
+							<p>
+								Automatically sorts your transactions for crystal-clear
+								insights.
+							</p>
+						</div>
 					</div>
-				</button>
 
-				<button className="flex items-center gap-3 bg-black text-white px-6 rounded-3xl h-14">
-					<Image
-						src="/playstore.svg"
-						alt="Download from App Store"
-						width={30}
-						height={30}
-						className="text-white"
-					/>
-					<div>
-						<p className="text-xs">GIT IT ON</p>
-						<Link href="/" className="text-xl font-bold">
-							Google Play
-						</Link>
+					<div className="flex flex-col justify-center items-center text-center gap-3">
+						<Target />
+						<div>
+							<h2 className="text-2xl font-semibold">Customizable Goals</h2>
+							<p>Set and track financial objectives that matter to you.</p>
+						</div>
 					</div>
-				</button>
+
+					<div className="flex flex-col justify-center items-center text-center gap-3">
+						<TrendingUp />
+						<div>
+							<h2 className="text-2xl font-semibold">Predictive Analysis</h2>
+							<p>
+								Get ahead of your finances with spending forecasts and alerts.
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<Image
-				src="/hero-mockup.webp"
-				alt="Hero Mockup"
-				width={384}
-				height={340}
-				className=""
-			/>
 		</section>
 	);
 }
+//COMMENT
